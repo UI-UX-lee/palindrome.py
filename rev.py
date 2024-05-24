461304ffe911354e6891541e620d1cdd1221885c
Lingesh <uiuxlee09@gmail.com>
2024-05-24 20:11:18 +0530

    initial commit

---
+++ rev.py
@@ -0,0 +1,6 @@
+mystr = input("Enter the String : ")
+rev = mystr[::-1]
+if mystr==rev:
+ print("Enter the string",mystr,"is palindrome")
+else:
+ print("Enter the string",mystr,"is not palindrome")
